cafc8f2f19455626507d8cbe7ec76dce99829b54
Maxim <supermax74.02@gmail.com>
2018-09-04 23:34:19 +0300

    Small changes

--- Test.py
+++ Test.py
@@ -2,10 +2,11 @@ import vk
 
 from VKPy import VKBot, Event
 
-TOKEN = input("Enter any token")
+TOKEN = input("Enter a token")
+GROUP_ID = int(input("Enter group id"))
 session = vk.Session(TOKEN)
 api = vk.API(session, v='5.80')
-bot = VKBot(api, 168852132)
+bot = VKBot(api, GROUP_ID)
 
 
 @bot.handle_message(func_text=lambda x: True)
